76b87cbb086e991045d2556d4a3632e2cc68872b
zullytamayom <zullytamayom@gmail.com>
2026-05-27 22:53:57 -0500

    ajustes PreAuthorize en products

--- src/main/java/com/historias_de_cafe/backend/controller/ProductController.java
+++ src/main/java/com/historias_de_cafe/backend/controller/ProductController.java
@@ -6,6 +6,7 @@ import com.historias_de_cafe.backend.service.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,6 +22,7 @@ public class ProductController {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<ProductResponseDTO> create(@Valid @RequestBody ProductRequestDTO dto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.create(dto));
     }
